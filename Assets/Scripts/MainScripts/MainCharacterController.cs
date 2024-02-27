97b893bf3a6ab95c0026e2c22fd670213bd60b61
Viktor Myrhill <viktor.myrhill@learnet.se>
2024-02-27 09:07:40 +0100

    MainCharacterController
    Kennet movment hugo interaction och viktor animation

---
+++ Assets/Scripts/MainScripts/MainCharacterController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour
+{
+    private float horizontal;
+    public bool isRunning = false; // Flag to indicate if the player is running
+    private float runningSpeed = 12f; // Speed when running
+    private float walkingSpeed = 8f; // Speed when walking
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+    public Vector2 boxSize = new Vector2(0.5f, 2f);
+
+    public Animator anim;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        // Toggle running
+        if (Input.GetKeyDown(KeyCode.LeftShift) && horizontal != 0)
+        {
+            isRunning = true;
+        }
+        else if (!Input.GetKey(KeyCode.LeftShift) || horizontal == 0)
+        {
+            isRunning = false;
+        }
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            CheckInteraction();
+        }
+
+        if (horizontal != 0)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        // Calculate movement speed
+        float currentSpeed = isRunning ? runningSpeed : walkingSpeed;
+        rb.velocity = new Vector2(horizontal * currentSpeed, rb.velocity.y);
+    }
+
+    private void CheckInteraction()
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(transform.position, boxSize, 0);
+
+        foreach (Collider2D collider in colliders)
+        {
+            Interaction interactionComponent = collider.GetComponent<Interaction>();
+
+            if (interactionComponent != null)
+            {
+                interactionComponent.Interact();
+                return;
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
