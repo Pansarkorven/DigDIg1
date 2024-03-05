3d472fb359fd84e5fa108f8665a42be86c5abe9e
Viktor Myrhill <viktor.myrhill@learnet.se>
2024-03-05 11:02:44 +0100

    FixatFLip,Bossgrejer

--- Assets/Scripts/Boss/BossFollowPlayer.cs
+++ Assets/Scripts/Boss/BossFollowPlayer.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class BossFollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform player;
+    public float moveSpeed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveTowardsPlayer();
+    }
+
+    void MoveTowardsPlayer()
+    {
+        if (player != null)
+        {
+            // Calculate the direction towards the player
+            Vector3 direction = (player.position - transform.position).normalized;
+
+            // Only move along the X-axis
+            direction.y = 0;
+
+            // Update the boss's position
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
     }
 }
