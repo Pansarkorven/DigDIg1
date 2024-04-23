5da150cacaff5906f6e69fa620381d9ad2d0ca28
Kenet <Kenneth.nguyen@elev.ga.lbs.se>
2024-04-23 10:54:00 +0200

    Revert "nya camera system"
    This reverts commit f97a8c9b37e0cadf7af7dd6ea4eb9cb52bd2028b.

--- Assets/VCam.cs
+++ Assets/VCam.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vcam : MonoBehaviour
+public class VCam : MonoBehaviour
 {
     [SerializeField] GameObject virtualCam;
 
