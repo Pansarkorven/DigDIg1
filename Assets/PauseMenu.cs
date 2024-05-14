8fc0792d4797fffb40f910311773cd18822dba4d
Kenet <Kenneth.nguyen@elev.ga.lbs.se>
2024-05-14 10:51:01 +0200

    Pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,60 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Options()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("ESC key pressed");
+            if (isPaused)
+            {
+                Debug.Log("Resuming game");
+                ResumeGame();
+            }
+            else if (isPaused)
+            {
+                Debug.Log("Pausing game");
+                PauseGame();
+            }
+            
+        }
+    }
+    void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+    }
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+    }
+}
